373cd7edf6d3778d97b61cc696b12231d239cb4b
dontfuu <phucnguyendong6603@gmail.com>
2025-11-02 18:50:29 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  darkMode: ["class", "media"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          light: "#ffffff",
+          dark: "#0d0d0d"
+        },
+        foreground: {
+          light: "#111111",
+          dark: "#e6e6e6"
+        }
+      },
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: { lg: "1100px" }
+      }
+    }
+  },
+  plugins: []
 };
